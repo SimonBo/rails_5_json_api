1670dc9185b8ed4b3737c21af9b7c97ecb1c7e66
Szymon Borucki <szymon.borucki@gmail.com>
2016-01-26 16:25:52 +0100

    add memcached support

--- app/serializers/rental_unit_serializer.rb
+++ app/serializers/rental_unit_serializer.rb
@@ -1,3 +1,4 @@
 class RentalUnitSerializer < ActiveModel::Serializer
+  cache key: 'rental_unit'
   attributes :id, :address, :rooms, :bathrooms, :price_cents
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,16 +13,16 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
-  else
-    config.action_controller.perform_caching = false
-    config.cache_store = :null_store
-  end
+  # if Rails.root.join('tmp/caching-dev.txt').exist?
+  #   config.action_controller.perform_caching = true
+  #   config.cache_store = :memory_store
+  #   config.public_file_server.headers = {
+  #     'Cache-Control' => 'public, max-age=172800'
+  #   }
+  # else
+  #   config.action_controller.perform_caching = false
+  #   config.cache_store = :null_store
+  # end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -32,6 +32,9 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+
+  config.cache_store = :mem_cache_store, "localhost"
+  config.action_controller.perform_caching = true
 
 
   # Raises error for missing translations
